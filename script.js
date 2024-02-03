ff43bdc10111d39c9cd7e14540cd5b679b4e9843
sumaiyashifa <134805287+sumaiyashifa@users.noreply.github.com>
2024-02-03 18:51:29 +0600

    message

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var typed = new Typed(".text", {
+  strings: ["an Engineer", "a Web Developer"],
+  typeSpeed: 100,  
+  backSpeed: 100,   
+  backDelay: 1000,
+  loop: true
+});
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
